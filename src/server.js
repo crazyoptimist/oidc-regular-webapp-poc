ef71ae88f13ebbd94746d26bb52773b49c7936e4
crazyoptimist <hey@crazyoptimist.net>
2023-08-25 14:09:32 -0500

    init: just read the code

---
+++ src/server.js
@@ -0,0 +1,139 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+const request = require('request-promise');
+const session = require('express-session');
+
+require('dotenv').config();
+
+const nonceCookieName = 'authorization-nonce';
+let oidcProviderInfo;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(crypto.randomBytes(16).toString('hex')));
+app.use(
+  session({
+    secret: crypto.randomBytes(32).toString('hex'),
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/profile', (req, res) => {
+  const { idToken, decodedIdToken } = req.session;
+  res.render('profile', {
+    idToken,
+    decodedIdToken
+  });
+});
+
+app.get('/login', (req, res) => {
+  const authorizationEndpoint = oidcProviderInfo['authorization_endpoint'];
+  const responseType = 'id_token';
+  const scope = 'openid';
+  const clientID = process.env.CLIENT_ID;
+  const redirectUri = 'http://localhost:3000/callback';
+  const responseMode = 'form_post';
+  const nonce = crypto.randomBytes(16).toString('hex');
+
+  const options = {
+    maxAge: 1000 * 60 * 15,
+    httpOnly: true,
+    signed: true
+  };
+
+  res
+    .cookie(nonceCookieName, nonce, options)
+    .redirect(
+      authorizationEndpoint +
+        '?response_mode=' +
+        responseMode +
+        '&response_type=' +
+        responseType +
+        '&scope=' +
+        scope +
+        '&client_id=' +
+        clientID +
+        '&redirect_uri=' +
+        redirectUri +
+        '&nonce=' +
+        nonce
+    );
+});
+
+app.post('/callback', async (req, res) => {
+  const nonce = req.signedCookies[nonceCookieName];
+
+  delete req.signedCookies[nonceCookieName];
+
+  const { id_token } = req.body;
+
+  const decodedToken = jwt.decode(id_token, {
+    complete: true
+  });
+
+  const kid = decodedToken.header.kid;
+
+  const client = jwksClient({
+    jwksUri: oidcProviderInfo['jwks_uri']
+  });
+
+  client.getSigningKey(kid, (err, key) => {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+
+    const verifiedToken = jwt.verify(id_token, signingKey);
+
+    const {
+      nonce: decodedNonce,
+      aud: audience,
+      exp: expirationDate,
+      iss: issuer
+    } = verifiedToken;
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    const expectedAudience = process.env.CLIENT_ID;
+
+    if (
+      audience !== expectedAudience ||
+      decodedNonce !== nonce ||
+      expirationDate < currentTime ||
+      issuer !== oidcProviderInfo['issuer']
+    ) {
+      return res.status(401).send();
+    }
+
+    req.session.decodedIdToken = verifiedToken;
+    req.session.idToken = id_token;
+
+    res.redirect('/profile');
+  });
+});
+
+const { OIDC_PROVIDER } = process.env;
+const discoveryEndpoint = `https://${OIDC_PROVIDER}/.well-known/openid-configuration`;
+request(discoveryEndpoint)
+  .then(res => {
+    oidcProviderInfo = JSON.parse(res);
+    app.listen(3000, () => {
+      console.log(`Server running on http://localhost:3000`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    console.error(`Unable to get OIDC endpoints for ${OIDC_PROVIDER}`);
+    process.exit(1);
+  });
